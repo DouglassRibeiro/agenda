67789cdc23c6223fcba1532e88449ebc8bf9fa08
DouglassRibeiro <douglasribeirodarosa@gmail.com>
2026-02-20 10:38:53 -0300

    fix: feat: added htmx version 1.9.10 (#2)

--- core/views.py
+++ core/views.py
@@ -4,4 +4,4 @@ def core_view(request):
     return render(request,'core/index.html')
 
 def teste_htmx(request):
-    return render (request,'core/partials/menssagen.html')
+    return render (request,'core/partials/menssagem.html')
